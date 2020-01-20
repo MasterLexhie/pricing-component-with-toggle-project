8fa1ebdc2e07a10ecb28a71cc73685d4cfc705ac
Alex Kanu <alex.kanu@SBTEL.local>
2020-01-20 14:10:16 +0100

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+const TOGGLE = document.querySelector("#toggle");
+const ANNUALLY = document.querySelector(".annual-price");
+const MONTHLY = document.querySelector(".monthly-price");
+
+const hideAnnualPrice = (ANNUALLY.classList.toggle("hide"));
+const hideMonthlyPrice = (MONTHLY.classList.toggle("hide"));
+
+// console.log({ ANNUALLY });
+
+// const annual =
+// const month =
+
+const toggleOptions = () => {
+ return TOGGLE ? 
+};
+
+// TOGGLE.addEventListener("onchange", toggleOptions());
